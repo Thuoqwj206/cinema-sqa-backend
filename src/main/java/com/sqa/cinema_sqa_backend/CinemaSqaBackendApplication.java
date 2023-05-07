5aa69c6acb2098557b9b044c7d1fee0c3c6bf394
Thuoqwj206 <phamvanthuog70@gmail.com>
2023-05-07 21:36:07 +0700

    Initial commit

---
+++ src/main/java/com/sqa/cinema_sqa_backend/CinemaSqaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sqa.cinema_sqa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSqaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSqaBackendApplication.class, args);
+    }
+
+}
